a828c4ca4e65321675d63df503b4da05bc8537f5
Dorie-xa <agabadoreck1@gmail.com>
2026-06-16 22:05:48 +0300

    lists

---
+++ Assignments.py/Lists.py
@@ -0,0 +1,24 @@
+names=["Peter","James","Doreck","Praise","Joseph"]
+print(names)
+
+new_name = "Sarah"
+names[0] = new_name
+print(names)
+
+names.append("John")
+print(names)
+
+add_name="Bathel"
+names[2] = add_name
+print(names)
+
+names.remove("Praise")
+print(names)
+print(names[-1])
+
+
+new_list=["dad","mum","girl","boy","daughter","son","aunt"]
+print(new_list[2])
+print(new_list[3])
+print(new_list[4])
+
