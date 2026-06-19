9d8f919a0ddd75f310d42c90fdeb32cf9046a1af
Dorie-xa <agabadoreck1@gmail.com>
2026-06-19 12:34:09 +0300

    added hackathon assignment

--- Assignments.py/Lists.py
+++ Assignments.py/Lists.py
@@ -20,7 +20,28 @@ print(names[-1])
 new_list=["dad","mum","girl","boy","daughter","son","aunt"]
 print(new_list[1:5])
 
-#list of countries
-countries=["Uganda", "Kenya","Tanzania"]
-New_countries=countries.copy()
+# list of countries
+countries=["Uganda", "Kenya", "Tanzania"]
+new_countries = countries.copy()
+print("Countries:", countries)
+print("Copied countries:", new_countries)
+
+for country in countries:
+    print(country)
+
+# list of animal names and sort them in ascending and descending order
+animals = ["zebra", "lion", "elephant", "giraffe", "antelope", "cat", "dog"]
+print("Animals ascending:", sorted(animals))
+print("Animals descending:", sorted(animals, reverse=True))
+
+#animal names containing the letter 'a'
+for animal in animals:
+    if "a" in animal:
+        print(animal)
+
+# join two lists 
+first_name = ["Doreck"]
+second_name = ["Agaba"]
+full_name = first_name + second_name
+print("Joined name list:", full_name)
 
